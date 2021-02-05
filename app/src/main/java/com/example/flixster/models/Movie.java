61bb4b0a86d1d2a6d0b75b66dcdc67d2b088cfc4
Athma-Vaishali <athmavaishali@gmail.com>
2021-02-05 15:46:09 -0600

    Flixster (adding details screen)

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -3,21 +3,49 @@ package com.example.flixster.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
     String backdropPath;
     String posterPath;
     String title;
     String overview;
+    double rating;
+    boolean adult;
+    String release_date;
+    int movie_id;
 
     public Movie(JSONObject jsonObject) throws JSONException {
         backdropPath=jsonObject.getString("backdrop_path");
         posterPath=jsonObject.getString("poster_path");
         title=jsonObject.getString("title");
         overview=jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        adult = jsonObject.getBoolean("adult");
+        release_date = jsonObject.getString("release_date");
+        movie_id=jsonObject.getInt("id");
+    }
+
+    public Movie(){}
+
+    public int getMovie_id() {
+        return movie_id;
+    }
+
+    public String getRelease_date() {
+        return release_date;
+    }
+
+    public boolean isAdult() {
+        return adult;
+    }
+
+    public double getRating() {
+        return rating;
     }
 
     public String getPosterPath() {
